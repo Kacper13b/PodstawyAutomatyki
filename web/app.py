575f871d6dc463493cd68bc74d3abc002a29b04c
Kacper13b <kacper.banasik13b@gmail.com>
2021-01-28 20:29:18 +0100

    dodano wykresy

--- web/app.py
+++ web/app.py
@@ -7,6 +7,8 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import plotly.express as px
 from backend.simulation import set_simulation_config
+from flask import redirect, url_for
+from backend.simulation import simulation
 
 app = Flask(__name__)
 
@@ -16,19 +18,19 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/simulation', methods=['POST', 'GET'])
+@app.route('/simulation', methods=['POST','GET'])
 def my_form_post():
-    #set_demo_config()
-    return render_template('simulation.html', title='simulation', data=config)
+    if request.method == 'POST':
+        temp = int(request.form['temperature_goal'])
+        ambient_temp = int(request.form['ambient_temperature'])
+        time = int(request.form['time'])
+        init_temp = int(request.form['initial_temperature'])
+        set_simulation_config(time, init_temp, temp, ambient_temp)
+        return redirect(url_for('result_web'))
+    else:
+        return render_template('simulation.html', title='simulation', data=config)
 
-@app.route('/simulation', methods=['POST', 'GET'])
-def aaa():
-    temp = int(request.form['temperature_goal'])
-    ambient_temp = int(request.form['ambient_temperature'])
-    time = int(request.form['time'])
-    init_temp = int(request.form['initial_temperature'])
-    set_simulation_config(time, init_temp, temp, ambient_temp)
-    print("AAAAA")
+
 
 
 @app.route('/demo', methods=['POST', 'GET'])
@@ -60,7 +62,33 @@ def demo_web():
     fig.write_html("web/templates/plots.html")
     return render_template("plots.html")
 
+@app.route('/result', methods=['POST', 'GET'])
+def result_web():
+    simulation()
+    fig = make_subplots(rows=2, cols=3)
 
+    fig.add_trace(go.Scatter(x=[i for i in range(config.get_simulation_cycles())], y=config.temperature_list,
+                             mode='lines',
+                             name='Temperatura', line=dict(color="orange", width=2)), row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=[i for i in range(config.get_simulation_cycles())], y=config.delivered_heat_list,
+                             mode='lines',
+                             name='Dostarczone ciepło', line=dict(color="red", width=2)), row=1, col=2)
+
+    fig.add_trace(go.Scatter(x=[i for i in range(config.get_simulation_cycles())], y=config.control_error_list,
+                             mode='lines',
+                             name='Uchyb', line=dict(color="blue", width=2)), row=1, col=3)
+    fig.add_trace(go.Scatter(x=[i for i in range(config.get_simulation_cycles())], y=config.control_quantity_list,
+                             mode='lines',
+                             name='Moc', line=dict(color="green", width=2)), row=2, col=1)
+    fig.add_trace(go.Scatter(x=[i for i in range(config.get_simulation_cycles())], y=config.heat_loss_list,
+                             mode='lines',
+                             name='Strata ciepła', line=dict(color="black", width=2)), row=2, col=2)
+    fig.update_layout(
+        title_text=str("Wykres")
+    )
+    fig.write_html("web/templates/plots.html")
+    return render_template("plots.html")
 
 
 if __name__ == '__main__':
